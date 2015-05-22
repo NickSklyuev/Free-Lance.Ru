38228335d20d480e7a6db688d521f69ad2ec3f68
Yuriy Gagarin <service.ru@mail.ru>
2015-05-23 04:11:00 +0500

    HelloAlex

--- app/src/main/java/trilodi/ru/free_lanceru/LoginActivity.java
+++ app/src/main/java/trilodi/ru/free_lanceru/LoginActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//Hello Alex
 
 public class LoginActivity extends ActionBarActivity {
 
