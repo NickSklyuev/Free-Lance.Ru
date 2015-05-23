482548052dc98b62ec78e366f81ce09af813a2b2
Yuriy Gagarin <service.ru@mail.ru>
2015-05-23 06:38:31 +0500

    23/05/15

--- app/src/main/java/trilodi/ru/free_lanceru/UI/LoginActivity.java
+++ app/src/main/java/trilodi/ru/free_lanceru/UI/LoginActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import trilodi.ru.free_lanceru.R;
-//dl;dsksd;lkvdvl
+//dl;dsksd;lkvdvghgfhl
 public class LoginActivity extends ActionBarActivity {
 
     @Override
